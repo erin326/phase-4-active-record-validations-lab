95938adf04287dcd6f32cbe83c5601cad1f0af01
erin <erinverkoulen93@gmail.com>
2021-11-06 12:29:39 -0500

    commit tree 76d9c305b5896fe418a1fa7e651bf952cde82ea5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :clickbait?
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+         /Secret/i,
+          /Top\d/i,
+           /Guess/i
+    ]
+    def clickbait?
+        if CLICKBAIT.none? { |name| name.match title }
+            errors.add(:title, "must be clickbait")
+        end
+      
+     
+    end
 end
+
+# if title == "True Facts"
+#     errors.add(:title, "invalid")
+# end
